df24bf7a9a116f9d71f3b3f8e431e7f8add8a082
azwad-chowdhury <chwdhury.azwad@gmail.com>
2023-09-03 14:54:42 +0600

    Layout page complete with responsiveness.

--- src/routes/layout/+page.svelte
+++ src/routes/layout/+page.svelte
@@ -1,1 +1,33 @@
-<h1 class="text-3xl font-bold">Layout Page !</h1>
+<main
+	class="grid grid-cols-4 lg:grid-cols-12 lg:grid-rows-6 overflow-scroll lg:min-h-screen md:min-h-[350vh] min-h-[350vh]"
+>
+	<div
+		class="col-span-4 row-span-3 grid-rows-1 lg:col-span-2 lg:row-span-3 lg:col-start-1 bg-blue-400"
+	/>
+	<div
+		class="col-span-4 row-span-3 grid-rows-4 lg:col-span-4 lg:row-span-5 lg:col-start-3 bg-rose-100"
+	/>
+	<div
+		class="col-span-4 row-span-3 grid-rows-7 lg:col-span-6 lg:row-span-2 lg:col-start-7 bg-cyan-400"
+	/>
+	<div
+		class="col-span-4 row-span-3 grid-rows-10 lg:col-span-2 lg:row-span-2 lg:col-start-1 bg-teal-400"
+	/>
+	<div
+		class="col-span-4 row-span-3 grid-rows-13 lg:col-span-4 lg:row-span-4 lg:col-start-7 lg:row-start-3 row-start-7 bg-yellow-100 grid place-content-center"
+	>
+		<div class="outer-square h-[300px] w-[300px]">
+			<div class="inner-circle bg-yellow-100 h-[200px] w-[200px]">
+				<!-- mountain figures -->
+				<div class="mountain-small" />
+				<div class="mountain-big" />
+			</div>
+		</div>
+	</div>
+	<div
+		class="col-span-4 row-span-3 grid-rows-16 lg:col-span-2 lg:row-span-4 lg:col-start-11 lg:row-start-3 bg-emerald-400"
+	/>
+	<div
+		class="col-span-4 row-span-3 grid-rows-19 lg:col-span-6 lg:row-span-1 lg:col-start-1 bg-fuchsia-400"
+	/>
+</main>
